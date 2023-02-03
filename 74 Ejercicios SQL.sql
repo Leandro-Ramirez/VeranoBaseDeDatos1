cb48ed635a591c94ad18dc89bfee1aa2a1e35d1d
Leandro-Kun <90274325+Leandro-Ramirez@users.noreply.github.com>
2023-02-03 09:01:53 -0600

    Add files via upload

---
+++ 74 Ejercicios SQL.sql
@@ -0,0 +1,264 @@
+-- 1) Mostrar toda la información sobre los cursos cuya tarifa es menor que 150.
+
+
+-- 2) Todas las filas en las que el nombre del curso es superior o igual alfabéticamente a RACIONALISMO.
+
+
+-- 3) Obtener sin valores repetidos el conjunto de todas las tarifas del los cursos
+
+
+-- 4) Obtener de la tabla personal ordenada por el nombre de los empleados
+
+
+-- 5) Obtener la identificación del departamento, titulo del empleado y salario para todos los miembros de la plantilla fijando como campo principal
+-- de ordenación el departamento en secuencia ascendente y como campo secundario de ordenación el salario en secuencia descendente.
+
+
+-- 6) Obtener el nombre y el nuevo salario de todos los miembros del personal cuyo nuevo salario 
+-- sea mayor que 25,000 suponiendo que todos los salarios iniciales se han reducido en 100
+
+
+-- 7) Obtener el nombre del curso y la descripción de todos los cursos cuya descripción comience por la palabra ES.
+
+
+-- 8) Obtener el numero de curso y tarifa de todos los cursos menor que 50 o mayor que 400
+
+
+-- 9) Listar cualquier curso que tenga una tarifa coincidente con algún numero del conjunto {12, 50,75,90,100,500}
+
+
+-- 10) Obtener toda la información disponible sobre cualquier curso que no tenga
+-- una tarifa coincidente con algún número del conjunto {12, 50,75,90,100,500}
+
+
+-- 11) Obtener toda la información sobre cualquier curso que tenga una descripción que comience con la cadena ‘PARA’
+
+
+-- 12) Visualizar los nombre los nombres, numero de ayudantes y numero de departamento para 
+-- todos los miembros de la facultad para os que se sabe si tienen o no ayudantes.
+
+
+-- 13) Visualizar toda la información sobre los curso de tres créditos ofrecidos por el departamento de Filosofía.
+
+
+-- 14) Visualizar toda la información sobre los cursos que tengan una tarifa entre 100 y 500, ambos inclusive.
+
+
+-- 15) Visualizar los salarios de todo el personal y del claustro de la facultad en una única tabla.
+
+
+-- 16) Visualizar los salarios de todo el personal y del claustro de la facultad en una única tabla sin eliminar valores duplicados en el resultado.
+
+
+-- 17) Obtener toda la información sobre los cursos gratuitos y toda la información sobre cualquier curso que ofrezca el departamento de Filosofía.
+
+
+-- 18) En tres consultas, visualizar todos los departamentos académicos que ofrezcan cursos de modo que no aparezcan valores duplicados en la salida
+-- de datos. Además, para cada curso con tarifa menor que 100, visualizar sin filas repetidas el departamento académico que ofrece ese curso 
+-- y el número de créditos concedidos. Por ultimo, visualizar el número de curso y el nombre de todos los cursos que tengan 
+-- una tarifa superior a 100, incluyendo en el resultado una tercera columna cuyo contenido sea CURSO CARO para cada fila.
+
+
+-- 19) En dos consultas, visualizar el numero de curso, nombre y créditos de cualquier curso que ofrezca el departamento de informática y 
+-- ciencias de la información ordenando el resultado en secuencias descendente por numero de curso, visualizar también la identificación
+-- de los departamentos y el nombre del curso ordenado por código de departamento y dentro de cada departamento por nombre de curso.
+
+
+-- 20) Obtener toda la información sobre los cursos que ofrezca el departamento de informática y ciencias de la información con tarifa nula. 
+-- Asimismo, obtener toda la información sobre cualquier curso que tenga una tarifa que se encuentre entre cero y cien
+
+
+-- 21) Obtener toda la información sobre los cursos de filosofía que ofrezcan 3 créditos y que tengan una tarifa 
+-- que se encuentre entre cero y cien. Asimismo obtener toda la información existente sobre cualquier curso 
+-- ofrecido por los departamentos de Informática y Ciencias de la Información o Filosofía.
+
+
+-- 22) Obtener toda la información sobre cualquier curso de Informática y Ciencias de la Información o cualquier curso de tarifa nula. 
+-- Asimismo, obtener toda la información existente sobre cualquier curso que tenga tarifa igual a 50, 100, 150 o 200.
+
+
+-- 23) Obtener toda la información existente sobre el nombre de los cursos y la identificación del departamento de todos los cursos que no sean 
+-- ofrecidos por el departamento de Informática y Ciencias de la Información. Obtener también el nombre y la identificación del departamento de 
+-- todos los cursos, con la excepción de aquellos ofrecidos por los departamentos de Informática y Ciencias de la Información y Filosofía.
+
+
+-- 24) Obtener toda la información existente sobre los cursos de Teología con tarifa nula o de cualquier curso 
+-- (independientemente de su departamento y tarifa) que tenga un valor de 6 créditos. Obtener también toda 
+-- la información existente sobre los cursos de Teología con tarifa nula o que valgan 6 créditos.
+
+
+-- 25) Obtener toda la información existente sobre los cursos que no pertenecen al departamento de Informática y Ciencias de la Información o sobre
+-- cualquier curso (independientemente de su departamento) que tenga tarifa nula y valga tres créditos. Obtener también toda la información 
+-- disponible sobre todas la filas de la tabla CURSO, exceptuando los cursos de Informática y Ciencias de la Información que tengan tarifa nula.
+
+
+-- 26) Obtener toda la información existente sobre los cursos que valgan 2, 6 o 9 créditos. Obtener también toda la información
+-- de todos los cursos que no pertenecen a los departamentos de Teología o Informática y Ciencias de la Información.
+
+
+-- 27) Obtener toda la información existente sobre los cursos cuya tarifa se encuentre entre 100 y 200, incluyendo 
+-- ambos valores. Obtener también toda la información de todos los cursos con tarifa menor que 100 o mayor que 200.
+
+
+-- 28) Obtener toda la información existente sobre los cursos cuyo nombre comience con la letra C, obtener también toda la información
+-- de todoslos cursos de los departamentos de Informática y Ciencias de la Información, Teología o Matemática que valgan
+-- tres ceditos y con tarifa entre 50 y 300 presentando el resultado ordenado por curso dentro de cada departamento.
+
+
+-- 29) Obtener la primera letra de todos los nombres de cursos de informática y Ciencias de la Información. Obtener también los caracteres que
+-- aparecen en las posiciones tercera, cuarta, quinta y sexta. Por ultimo mostrar los tutores del número de curso sin el primer carácter.
+
+
+-- 30) Obtener la longitud real de cada nombre de curso ofrecido por el departamento de Teología.
+
+
+-- 31) Obtener el nombre y el cargo de cualquier miembro del personal que no esté asignado aun departamento existente.
+
+
+-- 32) Obtener el nombre y el cargo de cualquier miembro del personal asignado a un departamento existente.
+
+ 
+-- 33) Obtener para cada departamento que ofrezca cursos, el identificador de ese departamento
+-- seguido del número, nombre y tarifa del departamento que organiza el curso con mayor tarifa.
+
+
+-- 34) Obtener el numero de curso, nombre y tarifa de cada curso cuya tarifa sea menor que todos los salarios de todos los miembros del personal.
+
+
+-- 35) Obtener el número de curso, nombre y tarifa de cada curso cuya tarifa exceda del salario del cualquier miembro del personal.
+
+
+-- 36) Obtener para cada departamento que ofrezca cursos, el identificador de departamento y la tarifa media de los cursos
+-- ofrecidos por el departamento, siempre y cuando sea mayor que la tarifa media de todas las tarifas de cursos.
+
+
+-- 37) Obtener el numero, cargo e identificador de cada departamento de cada miembro 
+-- del personal asignado a un departamento no existente (sin identificador)
+
+
+-- 38) Obtener el número y nombre de cualquier miembro de la facultad que sea jefe de cualquier departamento que ofrezca un curso de seis créditos.
+
+
+-- 39) Obtener el nombre y cargo de cada miembro del personal que trabaja en el edificio de Humanidades.
+
+
+-- 40) Obtener el número, nombre y tarifa de los cursos con tarifa mayor o igual que el sueldo de cualquier miembro del personal.
+
+
+-- 41) Obtener el número, nombre y tarifa de los cursos con tarifa menor que la media.
+
+
+-- 42) Obtener el número, nombre y tarifa de los cursos con tarifa mínima no nula.
+
+
+-- 43) Obtener el nombre de la base de datos actual
+
+
+-- 44) Obtener la fecha actual del sistema
+
+
+-- 45) Lista de los meses en que se efectuaron las matriculas
+
+
+-- 46) Hora de la matriculación de los alumnos, primeramente con horas y minutos y después con horas, minutos y segundos.
+
+
+-- 47) Presentar las ciudades donde se encuentras las oficinas con su nombre inicial, en mayúsculas, en minúsculas y el numero de caracteres.
+
+
+-- 48)Buscar aquellos números de curso que están en la tabla MATRICULA y en la tabla CLASE (operación intersección)
+
+
+-- 49) Presentar los números de curso que están en la tabla CLASE y no están en la tabla MATRICULA (operación diferencia).
+
+
+-- 50) Listar aquellos números de curso que están en la tabla MATRICULA o en la tabla CLASE (operación unión)
+
+
+-- 51) Visualizar el nombre e identificador de departamento de cualquier catedrático que tenga un numero 
+-- de profesores ayudantes mayor que la media de créditos para cursos ofrecidos por su departamento.
+
+
+-- 52) Visualizar el nombre, identificador de cada departamento y salario de aquellos miembros
+-- de la facultad cuyo salario es mayor que el salario medio de su departamento.
+
+
+-- 53) Visualizar el numero de estudiantes y la fecha de MATRICULA de todos los estudiantes que están
+-- matriculados de al menos un curso ofrecido por un departamento ubicado en el edificio Ciencias SC.
+
+
+-- 54) Visualizar el nombre y número de ayudantes para aquellos miembros de la facultad que
+-- tienen tantos ayudantes como el número de créditos ofrecidos por cualquier curso.
+
+
+-- 55) Visualizar el nombre y el identificador de departamento de cualquier miembro 
+-- del claustro asignado a un departamento que ofrezca un curso de 6 créditos
+
+
+-- 56) Visualizar el nombre y el identificador de departamento de cualquier miembro 
+-- del claustro asignado a un departamento que no ofrezca un curso de 6 créditos.
+
+
+-- 57) Visualizar el nombre y el departamento de cualquier miembro de la facultad que no esté impartiendo clases durante este semestre
+
+
+-- 58) Visualizar el número de curso y nombre en donde se halle registrado el estudiante 800.
+
+
+-- 59) Visualizar toda la información acerca de cualquier curso de Informática y Ciencias de la Información
+-- con una tarifa menor que el sueldo medio de cualquiera asignado al departamento de Teología.
+
+
+-- 60) Visualizar el nombre y el salario de cualquier empleado miembro del personal cuyo sueldo sea menor o igual que la máxima tarifa del curso
+
+
+-- 61) Visualizar el nombre, número, departamento y tarifa de los cursos que tienen la segunda tarifa mas cara sabiendo que la tarifa mas cara es 500.
+
+
+-- 62) Visualizar el número, nombre y departamento de los cursos con la mínima tarifa.
+
+
+-- 63) Hallar la tarifa media para aquellos departamentos en los que dicha tarifa media sea mayor que 100 y que ofrezcan menos de seis cursos.
+
+
+-- 64) Hallar las tarifas media, máxima y mínima por crédito dentro de cada departamento sólo para aquellos grupos con tarifa mínima positiva.
+
+
+-- 65) Pata todos los departamentos excepto el de Teología, que tengan una tarifa
+-- media de sus cursos mayor que 100, obtener su identificación y su tarifa media.
+
+
+-- 66) Para cada valor distinto de tarifa determinar el numero total de créditos para los cursos
+-- que tengas ese valor de tarifa. Ordenar el resultado en secuencia descendente por tarifa.
+
+
+-- 67) Para cada departamento que ofrezca determinar el valor medio de la tarifa de todos los cursos que tengas tres créditos
+-- ofrecidos por cada departamento. Visualizar la salida en secuencia ascendente por identificación de departamento.
+
+
+-- 68) Para cada departamento al que se hace referencia en la tabla PERSONAL formar un comité compuesto por dos miembros del personal del 
+-- departamento de modo que, para cada posible pareja de miembros del personal se visualice su código de departamento seguido de los 
+-- nombres de los miembros del personal. El resultado debe contener una fila por cada posible pareja de miembros del personal.
+
+
+-- 69) Para cualquier curso que tenga un miembro del personal disponible para ser tutor, visualizar el número, nombres
+-- y cargos de los miembros del personal que pueden servir de tutores para ese curso y la ubicación de sus respectivos
+-- edificios y despachos. Clasificar la salida por nombre de los miembros del personal y número de curso.
+
+
+-- 70) Para cada departamento que ofrece servicios de tutoría, visualizar el identificador de departamento 
+-- junto con la tarifa media de lo cursos que éste ofrece y el sueldo medio de los miembros del personal
+-- que pueden autorizar dichos cursos. Clasificar la salida por identificador de departamento.
+
+
+-- 71) Visualizar el nombre y cargo de cada miembro del personal que trabaje en el edificio de Humanidades.
+
+
+-- 72) Para cada curso con una tarifa superior a 175, mostrar el nombre del curso, tarifa y numero de facultativo 
+-- del jefe responsable del curso, visualizando la salida en orden ascendente por nombre de curso.
+
+
+-- 73) Para cada miembro del personal cuyo salario anual excede de 1000 visualizar su nombre, código de departamento y edificio de destino.
+
+
+-- 74) De todo el personal asignado a los actuales departamentos, seleccionar toda la información acerca del personal y sus respectivos departamentos.
